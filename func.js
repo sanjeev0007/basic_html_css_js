1af2777361c02af5892eb88638560a81682bbabd
Dhruv Varshney <47208019+dvdhruv@users.noreply.github.com>
2019-10-01 17:17:31 +0530

    Add files via upload

---
+++ func.js
@@ -0,0 +1,31 @@
+function add_defect()
+				{
+					var  defect_name= prompt("Enter defect name: ", "eg: File Not Found");
+					var  defect_id= prompt("Enter defect id: ", "eg: 404");
+				}
+
+				function add_user()
+				{
+					var  name= prompt("Enter  name: ", "eg: Mickey Mouse");
+					var  username= prompt("Enter username: ", "eg: Mickey123");
+					var  email= prompt("Enter email: ", "eg: Mickey123@gmail.com");
+					var  password= prompt("Enter password: ", "eg: ****");
+				}
+				
+				function assign_defect()
+				{
+					var defect_id= prompt("Enter defect id:","eg: 404");
+					var username= prompt("Enter username: ", "eg: Mickey123");
+				}
+
+				function update()
+				{
+					var username= prompt("Enter username: ", "eg: Mickey123");
+					var defect_id= prompt("Enter defect id:","eg: 404");
+					var status= prompt("Enter defect status:","eg: resolved");
+				}
+
+				function generate()
+				{
+					
+				}
